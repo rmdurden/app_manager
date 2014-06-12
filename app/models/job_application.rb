47c2328a2711e8b72a28fb61350fdf94a86e368d
rmdurden <rmdurden@ncsu.edu>
2014-06-12 10:45:53 -0400

    Make uniqueness case insensitive for email validation

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,6 +1,6 @@
 class JobApplication < ActiveRecord::Base
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, uniqueness: true, format: { with: email_regex }
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: email_regex }
   validates :name, presence: true, on: 'create'
   validates :email, presence: true, on: 'create'
   validates :answer1, presence: true, on: 'create'
